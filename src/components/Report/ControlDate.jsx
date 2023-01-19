4371eab0ad01e83a1a409df39f36348f15114bb6
ruzabaj <ruja.bajracharya@gmail.com>
2023-01-19 17:04:41 +0545

    improved: css added pagination flex

--- src/components/Report/ControlDate.jsx
+++ src/components/Report/ControlDate.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import SelectSearch from 'react-select-search';
+import 'react-select-search/style.css'
 
-const ControlDate = ({ startDate, endDate,  handleGenerateReport, setStartDate, setEndDate, outlet, handleChange }) => {
+const ControlDate = ({ startDate, endDate, handleGenerateReport, setStartDate, setEndDate, outlet, handleChange }) => {
     return (
         <div className='control-dates'>
             <div className='control-dates-flex'>
                 <div>
                     <label>Outlet Name:</label>
                     <div>
-                        <select name="outletName" id="outletName" className="input-style" onChange={handleChange}>
+                        {/* <select name="outletName" id="outletName" className="input-style" onChange={handleChange}>
                             <option value=""></option>
                             {outlet.map((outlets, index) => (
                                 <option value={outlets.outlet_Name} key={index} >{outlets.outlet_Name}</option>
                             ))}
-                        </select>
+                        </select> */}
+                        <SelectSearch
+                            options={outlet}
+                            defaultValue=""
+                            search
+                            placeholder="Select Outlet Name"
+                            onChange={(e)=>handleChange(e)}
+                        />
                     </div>
                 </div>
                 <div className='date-picker'>
@@ -32,7 +42,7 @@ const ControlDate = ({ startDate, endDate,  handleGenerateReport, setStartDate, 
                 </div>
             </div>
             <div className='btn-report'>
-                <button type='submit'onClick={handleGenerateReport}>
+                <button type='submit' onClick={handleGenerateReport}>
                     Generate Report
                 </button>
             </div>
--- src/components/Report/index.jsx
+++ src/components/Report/index.jsx
@@ -1,12 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect} from 'react'
 import "../../scss/table.scss";
-import "react-datepicker/dist/react-datepicker.css";
-import { IoFastFoodOutline, IoFastFood, IoPeopleOutline } from "react-icons/io5";
-import { TbNumbers } from "react-icons/tb";
-import { FcClock } from "react-icons/fc";
 import axios from 'axios';
-import ControlDate from './ControlDate';
 import moment from 'moment';
+import ControlDate from './ControlDate';
+import Information from './Information';
 import Pagination from './Pagination';
 
 const Report = () => {
@@ -25,7 +22,8 @@ const Report = () => {
 
     let url = process.env.REACT_APP_BASE_URL;
 
-    const { TakeAway_totalSales, TakeAway, Voids, DineIn, DineIn_totalSales, first_orderAt, last_orderAt, Guest_count, Operating_hours, Cooked, item_count } = information;
+    const { TakeAway_totalSales, TakeAway, Voids, DineIn, DineIn_totalSales, Cooked, cooking } = information;
+
     useEffect(() => {
         axios.get(`${url}/outlets`)
             .then((response) => {
@@ -104,8 +102,8 @@ const Report = () => {
 
     }
 
-    const handleChange = (event) => {
-        setOutletName(event.target.value)
+    const handleChange = (e) => {
+        setOutletName(e)
     }
 
     return (
@@ -118,84 +116,7 @@ const Report = () => {
                     <div className='show-outlet-name'>
                         <h4>{outletName}</h4>
                     </div>
-                    <div className='dashboard'>
-                        <div className="row">
-                            <div className="col dashboard-items">
-                                <TbNumbers className='icon' />
-                                <div className='padding-dashboard-items'>
-                                    <label>Total no.of items:</label>
-                                    <p>{item_count}</p>
-                                    {/* <label>Items per page:</label>
-                                    <p>{totalCount}</p> */}
-                                </div>
-                            </div>
-                            <div className="col dashboard-items">
-                                <IoFastFoodOutline className='icon' />
-                                <div className='padding-dashboard-items'>
-                                    <label>First Order At:</label>
-                                    <p>{first_orderAt}</p>
-                                </div>
-                            </div>
-                            <div className="col dashboard-items">
-                                <IoFastFood className='icon' />
-                                <div className='padding-dashboard-items'>
-                                    <label>Last Order At:</label>
-                                    <p>{last_orderAt}</p>
-                                </div>
-                            </div>
-                            <div className="col dashboard-items">
-                                <IoPeopleOutline className='icon' />
-                                <div className='padding-dashboard-items'>
-                                    <label>Total guest:</label>
-                                    <p>{Guest_count}</p>
-                                </div>
-                            </div>
-                            <div className="col dashboard-items">
-                                <FcClock className='icon' />
-                                <div className='padding-dashboard-items'>
-                                    <label>Operating Hours:</label>
-                                    <p>{Operating_hours}</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        {orders.map((element, index) => (
-                            <div key={index} className="information">
-                                <div className='show-table-num'>
-                                    <p>Table <span>{element.TableNum}</span></p>
-                                </div>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th>S.No</th>
-                                            <th>Quantity</th>
-                                            <th>Item Name</th>
-                                            <th>Ordered At</th>
-                                            <th>Completed At</th>
-                                            <th>Total Time</th>
-                                            <th>Average Prepared Time</th>
-                                            <th>Prepared Time Difference</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {element.items.map((item, index) => (
-                                            <tr key={index}>
-                                                <td>{index + 1}</td>
-                                                <td>{item.Quantity}</td>
-                                                <td>{item.ItemName}</td>
-                                                <td>{item.orderedAt}</td>
-                                                <td>{item.completedAt}</td>
-                                                <td>{item.TotalTime}</td>
-                                                <td>{item.AvgPrepTime}</td>
-                                                <td>{item.prepTimeDifference}</td>
-                                            </tr>
-
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        ))}
-                    </div>
+                    <Information orders={orders} information={information}/>
                     <div className='information-report'>
                         <div className='report-flex'>
                             <div className='report-info-two'>
@@ -222,6 +143,10 @@ const Report = () => {
                                 <div className='report-info'>
                                     <label>Cooked:</label>
                                     <span>{Cooked}</span>
+                                </div>
+                                <div className='report-info'>
+                                    <label>Cooking:</label>
+                                    <span>{cooking}</span>
                                 </div>
                             </div>
 
